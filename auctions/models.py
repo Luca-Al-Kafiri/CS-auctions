a5f061829060fcc606ca626c02f352aea114f182
Luca <lucaalkafiri@gmail.com>
2020-08-29 02:28:08 +0400

    default pic

--- auctions/models.py
+++ auctions/models.py
@@ -6,19 +6,15 @@ class User(AbstractUser):
     pass
 
 class Listing(models.Model):
-    def image():
-        return 'https://www.thebluediamondgallery.com/wooden-tile/images/auction.jpg'
-
     user = models.CharField(max_length=64)
     title = models.CharField(max_length=64)
     description = models.TextField()
     price = models.IntegerField()
     category = models.CharField(max_length=64)
-    link = models.CharField(max_length=350, default=image())
+    link = models.CharField(max_length=350, default='https://www.thebluediamondgallery.com/wooden-tile/images/auction.jpg')
     time = models.DateTimeField()
     closed = models.BooleanField(default=False)
     watch = models.ManyToManyField(User, blank=True, related_name='watch')
-
 
     def __str__(self):
         return self.title
